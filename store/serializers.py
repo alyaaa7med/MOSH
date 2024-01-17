4128bab38b748199257965bc598ee4c6f20006b3
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-01-17 14:15:44 +0200

    ModelSerializer For Small Code

--- store/serializers.py
+++ store/serializers.py
@@ -9,24 +9,38 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length= 255 )
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length= 255 )
-    unit_price = serializers.DecimalField(max_digits=6,decimal_places=2, source = 'price')# django will look for the same field name in Product model 
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length= 255 )
+#     unit_price = serializers.DecimalField(max_digits=6,decimal_places=2, source = 'price')# django will look for the same field name in Product model 
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')# define a method here and it will return a value to this field 
+#     # serializing the id of the related collection in product 
+#     collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+#     # serializing the string representation  of the related collection in product 
+#     collection = serializers.StringRelatedField()
+#     # serializing nested relationship  
+#     collection = CollectionSerializer()
+#     # serializing the hyperlink  to the relationship 
+#     collection = serializers.HyperlinkedRelatedField(
+#         query_set=Collection.objects.all(),
+#         view_name= 'collection-detail')
+
+
+
+
+#     def calculate_tax (self , prod:Product):
+#         return prod.price * Decimal(1.2) 
+
+
+class ProductSerializer(serializers.ModelSerializer): # extend model serializer to minimize the code 
+    model = Product 
+    fields= ['id','title','price','price_with_tax','collection']
+    # here only add fields not in the product model class ^_^
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')# define a method here and it will return a value to this field 
-    # serializing the id of the related collection in product 
-    collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
-    # serializing the string representation  of the related collection in product 
-    collection = serializers.StringRelatedField()
-    # serializing nested relationship  
-    collection = CollectionSerializer()
-    # serializing the hyperlink  to the relationship 
+    # if you do not write that ,then it will serialize the id of collection
     collection = serializers.HyperlinkedRelatedField(
         query_set=Collection.objects.all(),
         view_name= 'collection-detail')
-
-
-
 
     def calculate_tax (self , prod:Product):
         return prod.price * Decimal(1.2) 
